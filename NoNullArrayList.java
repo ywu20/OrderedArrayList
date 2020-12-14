e1b7437dba70d1a62c58af458d2afaba87d14a40
Yuqing Wu <ywu20@stuy.edu>
2020-12-13 20:23:51 -0500

    constructors, first add()

---
+++ NoNullArrayList.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class NoNullArrayList<T>  extends ArrayList<T>{
+  public NoNullArrayList(){
+    super();
+  }
+
+  public NoNullArrayList(int startCapacity){
+    super(startCapacity);
+  }
+
+  public boolean add(T e){
+    if(e==null){
+      throw new IllegalArgumentException();
+    }
+    else{
+      super.add(e);
+    }
+    return true;
+  }
+}
